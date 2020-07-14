ca79709582835892a0e067be2b793d94c44e84f0
xujiale <giliguala1024@qq.com>
2020-07-14 18:55:15 +0800

    使用thymeleaf 处理表格数据

--- src/main/java/com/xujiale/tools/controller/PageForwardController.java
+++ src/main/java/com/xujiale/tools/controller/PageForwardController.java
@@ -1,14 +1,21 @@
 package com.xujiale.tools.controller;
 
+import com.xujiale.tools.entity.DbStatus;
+import com.xujiale.tools.service.DbStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * @author xujiale 2020/7/10 19:35
  */
 @Controller
 public class PageForwardController {
-
+    @Autowired
+    DbStatusService dbStatusService;
 
     @GetMapping("/index")
     public String indexPage() {
@@ -20,5 +27,11 @@ public class PageForwardController {
         return "login.html";
     }
 
+    @GetMapping("/dblist")
+    public String getDbList(Model model) {
+        List<DbStatus> dbStatuses = dbStatusService.dbStatuses();
+        model.addAttribute("dbList", dbStatuses);
+        return "table";
+    }
 
 }
